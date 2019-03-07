56914a70c07d3b88050d8cf02ac69fe2be0a331b
Electrictoast <jkisor90@gmail.com>
2019-03-07 11:14:07 -0700

    uploading completed JTHD files

---
+++ JavaTheHardWay/WeaselOrNot.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class WeaselOrNot{
+	public static void main( String[] args){
+		Scanner keyboard=new Scanner(System.in);
+		String word;
+		Boolean yep, nope;
+		
+		System.out.println( "Type the word\"weasel\", please.");
+		word=keyboard.next();
+		yep= word.equals("weasel");
+		nope=!word.equals("weasel");
+		
+		System.out.println( "You typed what was requested: " + yep);
+		System.out.println( "you ignored polite instructions: "+nope);
+	}
+}
