e0fcf58fc7365497813dc1d851c7414cb3836dd1
Electrictoast <jkisor90@gmail.com>
2019-03-08 11:00:48 -0700

    objective 2

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3{
+  public static void main(String[] args) {
+    String food1,food2,food3;
+
+    food1="Breakfast!";
+    food2="Sushi";
+    food3="Burgers";
+
+    System.out.println("My favorit foods are:\n\t1. "+food1+"\n\t2. "+food2+"\n\t3. "+food3);
+
+  }
+}
---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,11 @@
+public class Objective2Lab4{
+  public static void main (String[] args){
+    double num1,num2,num3,average;
+
+    num1=532;
+    num2=411;
+    num3=1234.75;
+    average= (num1+num2+num3)/3;
+    System.out.println(average);
+  }
+}
